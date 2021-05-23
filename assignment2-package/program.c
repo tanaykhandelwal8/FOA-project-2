7b305d7da382baba3bef6f47192a868a99b35890
tanaykhandelwal8 <69707838+tanaykhandelwal8@users.noreply.github.com>
2021-05-24 03:48:37 +0530

    Stage 4 complete

--- assignment2-package/program.c
+++ assignment2-package/program.c
@@ -19,6 +19,7 @@
 #define NUMBER_OF_WORDS_IN_LETTER 22
 #define POS_TAGS_LENGTH 25         // (5 tags * 4 words per tag) + (4 spaces) + (1 '\0') = 25
 #define VARIATION_FORMS_LENGTH 105 //(4 forms * 25 letters) + (4 numbers) + (1 '\0') = 105
+#define WORDS_IN_SENTENCE_LENGTH 25
 
 /********** Type definitions ***********/
 
@@ -29,8 +30,16 @@ typedef struct
     char variation_forms[VARIATION_FORMS_LENGTH];
 } word_t;
 
+typedef struct {
+    char word[NUMBER_OF_WORDS_IN_LETTER];
+    char pos_tags[POS_TAGS_LENGTH];
+} stage3_t;
+
+typedef word_t dictionary_t[WORDS_IN_DICTIONARY];
+
+typedef char data_t[WORDS_IN_SENTENCE_LENGTH];
 typedef struct node node_t;
-typedef char data_t[NUMBER_OF_WORDS_IN_LETTER];
+
 struct node
 {
     data_t data;
@@ -43,20 +52,27 @@ typedef struct
     node_t *foot;
 } list_t;
 
-typedef word_t dictionary_t[WORDS_IN_DICTIONARY];
-
 /*********8** Helper function headers *********/
+// Stage 1
 void header(int stagenumber);
 void printstage1(word_t one_word);
+
+// Stage 2
 void printstage2(int numberofwords, float average_var_forms);
+
+// Stage 3
 list_t *create_empty_list();
-int getword(char W[], int limit);
-void print_list(list_t *mylist);
 void push(list_t *mylist, data_t new_item);
+void print_list(list_t *mylist);
+void free_list(list_t *mylist);
+int getword(char W[], int limit);
+data_t* get_head(list_t *list);
+int binary_search(dictionary_t dict, data_t word, int upper_bound);
 /************* Function headers ***********/
 word_t stage1();
-void stage2(dictionary_t dict);
+int stage2(dictionary_t dict);
 void stage3(dictionary_t dict);
+void stage4(dictionary_t dict);
 
 /*********** Stage 1 *****************/
 
@@ -99,56 +115,144 @@ void header(int stagenumber)
 }
 
 /***************** Stage 2 ****************/
-void stage2(dictionary_t dict) {
+int stage2(dictionary_t dict)
+{
     char ch;
     int counter = 0;
-    for(int i = 0; i < WORDS_IN_DICTIONARY; i++) {
-        if((ch=getchar()) != '*') {
+    for (int i = 0; i < WORDS_IN_DICTIONARY; i++)
+    {
+        if ((ch = getchar()) != '*')
+        {
             dict[counter++] = stage1();
         }
         else
             break;
     }
     int number_of_variation_forms = 0;
-    for(int i = 0; i < counter; i++) {
-        for(int j = 0; j < (int)strlen(dict[i].variation_forms); j++){
-            if(isdigit((int)dict[i].variation_forms[j])) 
-               number_of_variation_forms++;
+    for (int i = 0; i < counter; i++)
+    {
+        for (int j = 0; j < (int)strlen(dict[i].variation_forms); j++)
+        {
+            if (isdigit((int)dict[i].variation_forms[j]))
+                number_of_variation_forms++;
         }
     }
-    
     printstage1(dict[0]);
-    printstage2(counter, (number_of_variation_forms*1.0/counter));
+    printstage2(counter, (number_of_variation_forms * 1.0 / (counter)));
+
+    return counter;
 }
 
-
 /************* Stage 2 helper function **************/
-void printstage2(int number_of_words, float avg_var_forms) {
-    header(2); 
+void printstage2(int number_of_words, float avg_var_forms)
+{
+    header(2);
     printf("Number of words: %d\n", number_of_words);
     printf("Average number of variation forms per word: %.2f\n", avg_var_forms);
 }
 
-/************ Stage 3 ************/
+/*********** Stage 3 ***********************/
 
 void stage3(dictionary_t dict)
 {
+    int counter = stage2(dict);
+    list_t *words_in_sentence = create_empty_list();
     data_t word;
-    int counter = 0;
-    list_t* linkedlist_words = create_empty_list();
-    //char ch;
-    int a;
-    while((a=getword(word, NUMBER_OF_WORDS_IN_LETTER)) != EOF) {
-        // word is the word 'sea' 'sells' 'seashells'
-        // store the word in a linked list 
-        push(linkedlist_words, word);
-
+    int length_of_sentence = 0;
+    //creating the linked list using functions taken from get_n_prime.c
+    int a; // variable to check whether the function getword() has reached EOF
+    while((a=getword(word, WORDS_IN_SENTENCE_LENGTH)) != EOF) {
+        push(words_in_sentence, word);
+        length_of_sentence++;
     }
-    // linked list ready with the sentence
-    for(int i = 0; i < counter; i++)
-        printf("%s\n", dict[i].word);
+    int count=0;
+    stage3_t tags[length_of_sentence];
+    node_t* temp = words_in_sentence->head;
+    while(temp != NULL) {
+        int location;
+        if((location=binary_search(dict, temp->data, counter))) {
+            strcpy(tags[count].word,temp->data);
+            strcpy(tags[count++].pos_tags,dict[location].pos_tags);
+        }
+        else {
+            strcpy(tags[count].word, temp->data);
+            strcpy(tags[count++].pos_tags, "NOT_FOUND");
+        }
+        temp = temp->next;
+    }
+    header(3);
+    for(int i = 0; i < length_of_sentence; i++) {
+        printf("%s\t\t%-12s\n", tags[i].word, tags[i].pos_tags);
+    }
+
 }
-/****** Stage 3 Helper functions *********/
+
+
+/******* Stage 3 Helper functions ***********/
+int binary_search(dictionary_t dict, data_t word, int upper_bound) {
+        int bottom = 0; 
+        int top = upper_bound; 
+        int mid; 
+        while(bottom <= top) {
+            mid = (top+bottom)/2;
+
+            if(strcmp(dict[mid].word, word) == 0) {
+                return mid;
+            }
+            else if(strcmp(dict[mid].word, word) > 0) {
+                bottom = mid + 1;
+            }
+            else {
+                top = mid - 1;
+            }
+            
+        }
+        return 0;
+}
+list_t *create_empty_list()
+{
+    list_t *mylist = (list_t *)malloc(1 * sizeof(list_t));
+    assert(mylist);
+    mylist->head = mylist->foot = NULL;
+    return mylist;
+}
+
+void push(list_t *mylist, data_t new_item) {
+	node_t *new = (node_t *)malloc(1*sizeof(node_t));
+				
+	if (new == NULL) {
+		printf("Error!");
+		exit(EXIT_FAILURE);
+	}	
+    strcpy(new->data, new_item);
+	new->next = NULL;
+		
+	if (mylist->head == NULL) {
+		mylist->head = mylist->foot = new;
+
+	} else {
+		mylist->foot->next = new;
+		mylist->foot = new;	
+	}
+}
+
+data_t* get_head(list_t *list) {
+	assert(list!=NULL && list->head!=NULL);
+	return &(list->head->data);
+}
+
+void print_list(list_t *mylist) {
+	if(mylist->head) {
+		node_t *new = mylist->head;
+
+		while(new) {
+			printf("%s ", new->data);
+			new = new->next;
+		}
+		printf("\n");
+	}
+}
+
 int getword(char W[], int limit)
 {
     int c, len = 0;
@@ -173,52 +277,16 @@ int getword(char W[], int limit)
     return 0;
 }
 
+/*************** Stage 4 ********************/
+void stage4(dictionary_t dict) {
 
-list_t* create_empty_list(){
-	list_t* mylist = (list_t*)malloc(1*sizeof(list_t));
-	assert(mylist);
-	mylist->head = mylist->foot = NULL;
-	return mylist;
+
 }
-
-
-void print_list(list_t *mylist) {
-	if(mylist->head) {
-		node_t *new = mylist->head;
-
-		while(new) {
-			printf("%s ", new->data);
-			new = new->next;
-		}
-		printf("\n");
-	}
-}
-
-void push(list_t *mylist, data_t new_item) {
-	node_t *new = (node_t *)malloc(1*sizeof(node_t));
-				
-	if (new == NULL) {
-		printf("Error!");
-		exit(EXIT_FAILURE);
-	}	
-	strcpy(new->data, new_item);
-	new->next = NULL;
-		
-	if (mylist->head == NULL) {
-		mylist->head = mylist->foot = new;
-
-	} else {
-		mylist->foot->next = new;
-		mylist->foot = new;	
-	}
-}
-
 /************** Main/driver function *************/
 int main(int argc, char *argv[])
 {
-    dictionary_t t;
-    int* counter;
-    stage2(t);
-    stage3(t);
+    dictionary_t dict_of_words;
+    stage3(dict_of_words);
     
+    return 0;
 }
